ce44c77c4e67cd2353c8785188fac47c5a8820e5
cmoraes5 <moraescaio0502@gmail.com>
2022-06-25 16:26:37 -0300

    App

---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+export function Footer() {
+    return (
+        <footer className="w-full py-5 flex items-between justify-center bg-gray-700 border-t border-gray-600">
+                <div>
+                    <p>Created by <strong className="text-blue-500">CMVS</strong></p>
+                </div>
+        </footer>
+    )
+}
